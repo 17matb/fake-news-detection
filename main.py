3002822639db14be3e6fcc3eaa0854cb964e2f47
17matb <147427088+17matb@users.noreply.github.com>
2025-10-26 00:45:14 +0200

    feat: add argument parser with `--explore` flag and a message to help user if they do not provide any flag

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from argparse import ArgumentParser
+
+from data_handler.data_handler import DataHandler
+
+
 def main():
-    print("Hello from fake-news-detection!")
+    parser = ArgumentParser(prog="fake-news-detection")
+    parser.add_argument(
+        "-e",
+        "--explore",
+        action="store_true",
+        help="return data exploration",
+    )
+    arguments = parser.parse_args()
+    if not arguments.explore:
+        print(
+            "× Please use flags, you may want to read the help message. Use: uv run main.py -h"
+        )
+
+    if arguments.explore:
+        DataHandler(csv_path="./data/Fake.csv").load().explore()
 
 
 if __name__ == "__main__":
