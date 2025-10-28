6a210170ead6b541ea4286d9bf411a1739ac2834
github.com/nabil-ghazali <nabil.dev.ghaz@hotmail.com>
2025-10-28 16:57:14 +0100

    Function evaluation model and retrive

--- prompt/rag_system.py
+++ prompt/rag_system.py
@@ -1,6 +1,9 @@
 from .prompt_builder import PromptBuilder
 from chroma.chroma_manager import ChromaManager
-
+import pandas as pd
+import re
+from collections import Counter
+from sklearn.metrics import accuracy_score
 
 class RAGSystem:
     def __init__(self, collection_name="news", model_lm="phi3:3.8b"):
@@ -35,5 +38,47 @@ class RAGSystem:
         
         except Exception as e:
             return f"Erreur lors de l'analyse: {e}"
+    
+    def eavaluation_rag(self, search_results, response):
+        # search_results est le retour de self.chroma_manager.query(article_text, n_results=3) / def analyse_aticle
+        retrieved_chunks = []
+
+        for doc, meta, dist in zip(
+            search_results['documents'][0],
+            search_results['metadatas'][0],
+            search_results['distances'][0]
+        ):
+            retrieved_chunks.append({
+                "text": doc,
+                "subject": meta['subject'],
+                "date": meta['date'],
+                "label": meta['label'],   # vrai label
+                "distance": dist
+            })
+
+        # Convertir en DataFrame pour plus de clarté
+        df_chunks = pd.DataFrame(retrieved_chunks)
+        pass
+                
+        # Réponse du LLM
+        llm_text = response["response"]
+
+        # Extraire le Label entre les ```plaintext``` ou après "Label :"
+        match = re.search(r"Label\s*:\s*(True|Fake)", llm_text, re.IGNORECASE)
+        if match:
+            predicted_label = match.group(1)
+        else:
+            predicted_label = None
+
+        # Labels réels des chunks récupérés
+        true_labels = df_chunks["label"].tolist()
 
 
+        # Prend la majorité des labels 
+        majority_label = Counter(true_labels).most_common(1)[0][0]
+
+        # Comparaison
+        accuracy = int(predicted_label.lower() == majority_label.lower())
+        print(f"Predicted label: {predicted_label}")
+        print(f"Majority label of retrieved chunks: {majority_label}")
+        print(f"Test passed? {accuracy == 1}")
