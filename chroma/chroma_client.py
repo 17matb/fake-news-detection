5bcdfcf4c00655589263aa10d3bf0723cfc588d3
17matb <147427088+17matb@users.noreply.github.com>
2025-10-28 15:26:16 +0100

    feat: provide `chroma_insertion` method in `Pipeline` and make sure everything needed is working
    also fix stuff that made `add_dataframe_to_collection` process slow by
    making sure it does not call the ollama api for every single chunk

--- chroma/chroma_client.py
+++ chroma/chroma_client.py
@@ -1,6 +1,7 @@
 import chromadb
 from chromadb.utils import embedding_functions
-from singleton import SingletonMeta
+
+from chroma.singleton import SingletonMeta
 
 
 class ChromaClient(metaclass=SingletonMeta):
@@ -9,7 +10,7 @@ class ChromaClient(metaclass=SingletonMeta):
     """
 
     def __init__(
-        self, db_path: str = "./chroma_db", model_name: str = "all-minilm:latest"
+        self, db_path: str = "./chroma_db/", model_name: str = "all-minilm:latest"
     ):
         self.client = chromadb.PersistentClient(path=db_path)
         self.embedding_function = embedding_functions.OllamaEmbeddingFunction(
@@ -36,3 +37,4 @@ class ChromaClient(metaclass=SingletonMeta):
             name=name,
             embedding_function=self.embedding_function,  # pyright: ignore[reportArgumentType]
         )
+
--- main.py
+++ main.py
@@ -12,14 +12,23 @@ def main():
         action="store_true",
         help="return data exploration",
     )
+    parser.add_argument(
+        "-i",
+        "--insert",
+        action="store_true",
+        help="process data and insert everything into chromadb",
+    )
     arguments = parser.parse_args()
-    if not arguments.explore:
+    if not arguments.explore and not arguments.insert:
         print(
             "× Please use flags, you may want to read the help message. Use: uv run main.py -h"
         )
 
     if arguments.explore:
         pipeline.data_exploration()
+    if arguments.insert:
+        pipeline.chroma_insertion()
+
 
 if __name__ == "__main__":
     main()
