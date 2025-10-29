4a303afdfff8909dfc8449be8a1c117eb73d5527
github.com/nabil-ghazali <nabil.dev.ghaz@hotmail.com>
2025-10-29 10:06:27 +0100

    function evaluation RAG

--- prompt/rag_system.py
+++ prompt/rag_system.py
@@ -3,7 +3,6 @@ from chroma.chroma_manager import ChromaManager
 import pandas as pd
 import re
 from collections import Counter
-from sklearn.metrics import accuracy_score
 
 class RAGSystem:
     def __init__(self, collection_name="news", model_lm="phi3:3.8b"):
@@ -39,7 +38,7 @@ class RAGSystem:
         except Exception as e:
             return f"Erreur lors de l'analyse: {e}"
     
-    def eavaluation_rag(self, search_results, response):
+    def evaluation_rag(self, search_results, response):
         # search_results est le retour de self.chroma_manager.query(article_text, n_results=3) / def analyse_aticle
         retrieved_chunks = []
 
@@ -66,7 +65,7 @@ class RAGSystem:
         # Extraire le Label entre les ```plaintext``` ou après "Label :"
         match = re.search(r"Label\s*:\s*(True|Fake)", llm_text, re.IGNORECASE)
         if match:
-            predicted_label = match.group(1)
+            predicted_label = match.group(1) # group(1) renvoie juste la partie du groupe capturant ("True").
         else:
             predicted_label = None
 
@@ -75,10 +74,26 @@ class RAGSystem:
 
 
         # Prend la majorité des labels 
-        majority_label = Counter(true_labels).most_common(1)[0][0]
+        # Counter est une classe du module standard Python collections
+        majority_label = Counter(true_labels).most_common(1)[0][0] # Counter sert à compter les occurrences de chaque élément dans une liste.
+                                                                   # most_common extrait le premier élément du tuple,
+        
+        
+        # Comparaison du label prédit avec les labels des chunks
+        matches = sum(1 for label in true_labels if label.lower() == predicted_label.lower())
+        percentage = (matches / len(true_labels)) * 100
 
-        # Comparaison
-        accuracy = int(predicted_label.lower() == majority_label.lower())
-        print(f"Predicted label: {predicted_label}")
-        print(f"Majority label of retrieved chunks: {majority_label}")
-        print(f"Test passed? {accuracy == 1}")
+        # -------------------- A supprimer apres test fonction --------------------------
+
+        # accuracy = int(predicted_label.lower() == majority_label.lower())
+
+        # print(f"Predicted label: {predicted_label}")
+        # print(f"Labels des chunks: {true_labels}")
+        # print(f"Correspondances: {matches}/{len(true_labels)} ({percentage:.2f}%)")
+    
+        # # Affichage des comparaisons
+        # print(f"Predicted label: {predicted_label}")
+        # print(f"Majority label of retrieved chunks: {majority_label}")
+        # print(f"Test passed? {accuracy == 1}")
+
+        return percentage
